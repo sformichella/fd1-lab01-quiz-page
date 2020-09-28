d23b3d2a13e47fdbf26d858d20bec1c1e4dc7f0e
sformichella <sformichella@gmail.com>
2020-09-28 15:25:21 -0700

    add tests. whoops! shoulve done this earlier!

---
+++ apps/app.js
@@ -0,0 +1,9 @@
+import{ beginQuizClickHandler } from './clickHandlers.js';
+
+
+
+
+
+let beginQuizButton =  document.getElementById('quiz-button');
+
+beginQuizButton.addEventListener('click', beginQuizClickHandler);
---
+++ apps/clickHandlers.js
@@ -0,0 +1,60 @@
+import{
+    yesCoerce, 
+    evaluationMessage
+} from './utils.js';
+
+
+
+
+let results = document.getElementById('results');
+let evaluationText =  document.getElementById('eval-text');
+
+export function beginQuizClickHandler() {
+
+// Confirm that the user wants to start the quiz
+    if (!confirm('Are you sure you want to start the quiz?')) {
+        return;
+    }
+
+// Variables
+    let correctAnswers = 0;
+    let percentage
+    const userName = prompt('Please enter your name:');
+
+
+// Question One
+    const questionOneResponse = prompt('Was Euclid an Indian mathematician?');
+
+    if (!yesCoerce(questionOneResponse)) {
+        correctAnswers++;
+    }
+
+
+// Question Two
+    const questionTwoResponse = prompt('Is there an infinite amount of prime numbers?');
+
+    if (yesCoerce(questionTwoResponse)) {
+        correctAnswers++;
+    }
+
+
+// Question Three
+    const questionThreeResponse = prompt('Is the square root of ten an irrational number?');
+
+    if (yesCoerce(questionThreeResponse)) {
+        correctAnswers++;
+    }
+
+
+// After quiz stuff
+
+    alert('The quiz is now complete! Your results will be display below.');
+
+    percentage = Math.floor(100 * correctAnswers/3);
+
+    results.textContent = `${userName} got ${correctAnswers} answers correct out of 3. That's ${percentage}%`;
+
+    evaluationText.textContent = evaluationMessage(percentage);
+
+    correctAnswers = 0;
+}
